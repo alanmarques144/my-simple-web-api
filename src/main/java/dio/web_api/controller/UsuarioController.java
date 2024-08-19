5ddd2fd6f5b4b64ab581e8a07a24115a1ba0075a
alanmarques144 <alanshalom12@gmail.com>
2024-08-19 15:13:42 -0300

    criacao dos models e repositories

---
+++ src/main/java/dio/web_api/controller/UsuarioController.java
@@ -0,0 +1,5 @@
+package dio.web_api.controller;
+
+public class UsuarioController {
+    
+}
--- src/main/java/dio/web_api/controller/WelcomeController.java
+++ src/main/java/dio/web_api/controller/WelcomeController.java
@@ -1,5 +1,13 @@
 package dio.web_api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WelcomeController {
     
+    @GetMapping
+    public String welcome(){
+        return "Welcome to my spring boot API!";
+    }
 }
